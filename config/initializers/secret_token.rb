ca1943465fc4c2838726a75a16643b9033932a8d
Ali Ahmed El-Kadim <aliahmed454@gmail.com>
2013-11-26 17:03:59 +0200

    Created a reails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubytest::Application.config.secret_token = '245d49037558ffff8da6819888156628719a8df805e0be3db3486ee8ba931257e99a47a4a3607fea612bccf7523877316178747a0229d5a910436fb2ae24da47'
